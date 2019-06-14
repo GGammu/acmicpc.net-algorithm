7510a50532ecfe295521fd75e5439d549d487bff
Young Hwang <ykcul02@gmail.com>
2019-06-14 14:30:04 +0900

    6 days

--- 05-03-2448/src/Main.java
+++ 05-03-2448/src/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
@@ -35,10 +39,49 @@ public class Main {
 		 * ***** ***** ***** ***** ***** ***** ***** *****	
 		 */
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		int N = Integer.parseInt(br.readLine());
+		int lineCnt = N / 3;
+		char[][] stars = new char[N][N * 2 - 1];
+		
+		for (int i = 0; i < stars.length; i++) {
+			for (int j = 0; j < stars[0].length; j++) {
+				stars[i][j] = ' ';
+			}
+		}
+		
+		drawTri(stars, lineCnt, 0, (N * 2 - 1) / 2);
+		
+		for (int i = 0; i < stars.length; i++) {
+			for (int j = 0; j < stars[0].length; j++) {
+				bw.write(stars[i][j]);
+			}
+			bw.newLine();
+		}
+		
+		bw.flush();
 	}
 	
-	private static void draw(int x, int y) {
-		
+	private static void drawTri(char[][] stars, int n, int y, int x) {
+		if (n == 1) {
+			drawStar(stars, y, x);
+		} else {
+			drawTri(stars, n / 2, y, x);
+			drawTri(stars, n / 2, y + (3 * n / 2), x - (3 * n / 2));
+			drawTri(stars, n / 2, y + (3 * n / 2), x + (3 * n / 2));
+		}
+	}
+
+	private static void drawStar(char[][] stars, int y, int x) {
+		stars[y][x] = '*';
+		stars[y + 1][x - 1] = '*';
+		stars[y + 1][x + 1] = '*';
+		stars[y + 2][x - 2] = '*';
+		stars[y + 2][x - 1] = '*';
+		stars[y + 2][x] = '*';
+		stars[y + 2][x + 1] = '*';
+		stars[y + 2][x + 2] = '*';
 	}
 }
