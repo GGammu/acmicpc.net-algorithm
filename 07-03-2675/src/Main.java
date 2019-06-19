a80ac182224f45d322c5b102b5df621f26b207c1
Young Hwang <ykcul02@gmail.com>
2019-06-20 07:31:55 +0900

    9days

--- 07-03-2675/src/Main.java
+++ 07-03-2675/src/Main.java
@@ -1,7 +1,11 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		/*
 		 * 문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
 		 * QR Code "alphanumeric" 문자는 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./: 이다.
@@ -16,7 +20,30 @@ public class Main {
 		 * /////HHHHHTTTTTPPPPP
 		 */
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		int lineCnt = Integer.parseInt(br.readLine());
+		String[] rows = new String[lineCnt];
+		String[] tmp = null;
+		
+		for (int i = 0; i < lineCnt; i++) {
+			rows[i] = br.readLine();
+		}
+		
+		for (int i = 0; i < rows.length; i++) {
+			tmp = rows[i].split(" ");
+			
+			for (int j = 0; j < tmp[1].length(); j++) {
+				for (int k = 0; k < Integer.parseInt(tmp[0]); k++) {
+					bw.write(tmp[1].charAt(j));
+				}
+			}
+			
+			bw.newLine();
+		}
+		
+		bw.flush();
 	}
 	
 }
