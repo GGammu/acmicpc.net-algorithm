6e1ec45bedd42fd7fe8ad651c6ad8b65355462b7
Young Hwang <ykcul02@gmail.com>
2019-06-28 15:28:15 +0900

    13 days

--- 11021/src/Main.java
+++ 11021/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		 * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 		 * 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
 		 * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
-		 * 각 테스트 케이스마다 A+B를 출력한다.
+		 * 각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
 		 * 
 		 * 5
 		 * 1 1
@@ -16,20 +16,21 @@ public class Main {
 		 * 9 8
 		 * 5 2
 		 * 
-		 * 2
-		 * 5
-		 * 7
-		 * 17
-		 * 7
+		 * Case #1: 2
+		 * Case #2: 5
+		 * Case #3: 7
+		 * Case #4: 17
+		 * Case #5: 7
 		 */
 		
 		Scanner sc = new Scanner(System.in);
+		
 		int cnt = Integer.parseInt(sc.nextLine());
-		int A = 0;
-		int B = 0;
 		
 		String[] values = new String[cnt];
 		String[] nums = new String[2];
+		int A = 0;
+		int B = 0;
 		
 		for (int i = 0; i < cnt; i++) {
 			values[i] = sc.nextLine();
@@ -39,7 +40,7 @@ public class Main {
 			nums = values[i].split(" ");
 			A = Integer.parseInt(nums[0]);
 			B = Integer.parseInt(nums[1]);
-			System.out.println(A + B);
+			System.out.println("Case #" + (i + 1) + ": " + (A + B));
 		}
 	}
 	
