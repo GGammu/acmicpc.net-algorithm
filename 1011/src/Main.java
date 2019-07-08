d134a8a4e228cd76e4a9f06254e77f98d0d46509
ggammu <ykcul02@gmail.com>
2019-07-08 21:43:04 +0900

    29 days

--- 1011/src/Main.java
+++ 1011/src/Main.java
@@ -1,7 +1,12 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		/*
 		 * 우현이는 어린 시절, 지구 외의 다른 행성에서도 인류들이 살아갈 수 있는 미래가 오리라 믿었다. 그리고 그가 지구라는 세상에 발을 내려 놓은 지 23년이 지난 지금, 세계 최연소 ASNA 우주 비행사가 되어 새로운 세계에 발을 내려 놓는 영광의 순간을 기다리고 있다.
 		 * 그가 탑승하게 될 우주선은 Alpha Centauri라는 새로운 인류의 보금자리를 개척하기 위한 대규모 생활 유지 시스템을 탑재하고 있기 때문에, 그 크기와 질량이 엄청난 이유로 최신기술력을 총 동원하여 개발한 공간이동 장치를 탑재하였다. 하지만 이 공간이동 장치는 이동 거리를 급격하게 늘릴 경우 기계에 심각한 결함이 발생하는 단점이 있어서, 이전 작동시기에 k광년을 이동하였을 때는 k-1 , k 혹은 k+1 광년만을 다시 이동할 수 있다. 예를 들어, 이 장치를 처음 작동시킬 경우 -1 , 0 , 1 광년을 이론상 이동할 수 있으나 사실상 음수 혹은 0 거리만큼의 이동은 의미가 없으므로 1 광년을 이동할 수 있으며, 그 다음에는 0 , 1 , 2 광년을 이동할 수 있는 것이다. ( 여기서 다시 2광년을 이동한다면 다음 시기엔 1, 2, 3 광년을 이동할 수 있다. )
@@ -20,6 +25,49 @@ public class Main {
 		 * 4
 		 */
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		String[] value = new String[2];
+		long X = 0;
+		long Y = 0;
+		long tmp;
+		long distance = 0;
+		long speed = 0;
+		long lastspeed = 0;
+		long cnt = 1;
+
+		for (int i = 0; i < T; i++) {
+			value = br.readLine().split(" ");
+			
+			X = Long.parseLong(value[0]);
+			Y = Long.parseLong(value[1]);
+			
+			distance = Y - X;
+			speed = 0;
+			lastspeed = 0;
+			
+			while(true) {
+				speed++;
+				tmp = (long)Math.pow(speed, 2);
+				
+				if (distance >= tmp) {
+					lastspeed = speed;
+					continue;
+				} else {
+					cnt = 2 * lastspeed - 1 + (long)Math.ceil((distance - Math.pow(lastspeed, 2)) / lastspeed);
+					bw.write(String.valueOf(cnt));
+					break;
+				}
+			}
+
+			bw.newLine();
+		}
+		
+		bw.flush();
+
 	}
 	
 }
