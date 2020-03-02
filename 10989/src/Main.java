dcac4d832ac34d2f1e75398e003e507dd5c72f77
Young Hwang <ykcul02@gmail.com>
2020-03-02 22:10:34 +0900

    소팅 활용
    소팅 활용

---
+++ 10989/src/Main.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		문제
+//		N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+//
+//		입력
+//		첫째 줄에 수의 개수 N(1 ≤ N ≤ 10,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 이 수는 10,000보다 작거나 같은 자연수이다.
+//
+//		출력
+//		첫째 줄부터 N개의 줄에 오름차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
+//
+//		예제 입력 1 
+//		10
+//		5
+//		2
+//		3
+//		1
+//		4
+//		2
+//		3
+//		5
+//		1
+//		7
+//
+//		예제 출력 1 
+//		1
+//		1
+//		2
+//		2
+//		3
+//		3
+//		4
+//		5
+//		5
+//		7		
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		try {
+			int N = Integer.parseInt(br.readLine());
+
+			Integer[] nums = new Integer[N];
+
+			int max = 0;
+
+			for (int i = 0; i < N; i++) {
+				nums[i] = Integer.parseInt(br.readLine());
+				if (max < nums[i])
+					max = nums[i];
+			}
+
+			Integer[] count = new Integer[max + 1];
+			Integer[] sorted = new Integer[N];
+
+			for (int i = 0; i < max + 1; i++) {
+				count[i] = 0;
+			}
+			
+			for (int i = 0; i < N; i++) {
+				count[nums[i]] += 1;
+			}
+
+			for (int i = 0; i < count.length; i++) {
+				for (int j = 0; j < count[i]; j++) {
+					bw.write(String.valueOf(i));
+					bw.newLine();
+				}
+			}
+			
+			bw.flush();
+		} catch (Exception e) {
+		}
+	}
+
+}
